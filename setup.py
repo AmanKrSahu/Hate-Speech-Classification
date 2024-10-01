598a510ea1634e0d081a827e6a18e34ebdecf9db
Aman <amankrsahu003@gmail.com>
2024-10-02 02:53:05 +0530

    Added Project Requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Hate-Speech-Classification",
+    version="0.0.1",
+    author="Aman Kumar Sahu",
+    author_email="amankrsahu003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
