137b30abb90eed397a54f4f27152b009419f0ba0
Aman <amankrsahu003@gmail.com>
2024-10-06 22:07:06 +0530

    Added Data Transformation component

--- SpeechDetection/pipeline/train_pipeline.py
+++ SpeechDetection/pipeline/train_pipeline.py
@@ -2,12 +2,23 @@ import sys
 from SpeechDetection.logger import logging
 from SpeechDetection.exception import CustomException
 from SpeechDetection.components.data_ingestion import DataIngestion
-from SpeechDetection.entity.config_entity import (DataIngestionConfig)
-from SpeechDetection.entity.artifact_entity import (DataIngestionArtifacts)
+from SpeechDetection.components.data_transformation import DataTransformation
+
+from SpeechDetection.entity.config_entity import (
+    DataIngestionConfig,
+    DataTransformationConfig
+)
+
+from SpeechDetection.entity.artifact_entity import (
+    DataIngestionArtifacts,
+    DataTransformationArtifacts
+)
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_transformation_config = DataTransformationConfig()
+
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
         logging.info("Entered the start_data_ingestion method of TrainingPipeline class")
@@ -26,11 +37,33 @@ class TrainingPipeline:
         except Exception as e:
             raise CustomException(e, sys) from e
         
+    def start_data_transformation(self, data_ingestion_artifacts = DataIngestionArtifacts) -> DataTransformationArtifacts:
+        logging.info("Entered the start_data_transformation method of TrainPipeline class")
+        
+        try:
+            data_transformation = DataTransformation(
+                data_ingestion_artifacts = data_ingestion_artifacts,
+                data_transformation_config = self.data_transformation_config
+            )
+
+            data_transformation_artifacts = data_transformation.initiate_data_transformation()
+            
+            logging.info("Exited the start_data_transformation method of TrainPipeline class")
+            
+            return data_transformation_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
     def run_pipeline(self):
         logging.info("Entered the run_pipeline method of TrainingPipeline class")
 
         try:
             data_ingestion_artifacts = self.start_data_ingestion()
+
+            data_transformation_artifacts = self.start_data_transformation(
+                data_ingestion_artifacts = data_ingestion_artifacts
+            )
 
             logging.info("Exited the run_pipeline method of TrainingPipeline class")
 
